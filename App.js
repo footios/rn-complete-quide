66d1185ce4b6867428e180d2f4b3f0ed0b84ad3e
Fotis Tsakiris <footios76@gmail.com>
2019-08-22 01:07:20 +0300

    check for no input

--- App.js
+++ App.js
@@ -9,6 +9,9 @@ export default function App() {
 	const [ isAddMode, setIsAddMode ] = useState(false);
 
 	const addGoalHandler = (goalTitle) => {
+		if (goalTitle.length === 0) {
+			return
+		}
 		// It will render the comp only once!
 		setCourseGoals((currentGoals) => [ ...currentGoals, { id: Math.random().toString(), value: goalTitle } ]);
 		setIsAddMode(false)
